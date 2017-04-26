6cb91fa6a1b18cd0b70fd60615ac2ab197c57bfd
hashstudio <hashstudio@mail.ru>
2017-04-26 12:15:25 +0300

    Checkbox list added

---
+++ src/Phact/Form/Fields/CheckboxListField.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Okulov Anton
+ * @email qantus@mail.ru
+ * @version 1.0
+ * @company HashStudio
+ * @site http://hashstudio.ru
+ * @date 02/08/16 08:22
+ */
+
+namespace Phact\Form\Fields;
+
+use Phact\Orm\Manager;
+use Phact\Orm\Model;
+
+class CheckboxListField extends Field
+{
+    /**
+     * @var string
+     */
+    public $inputTemplate = 'forms/field/checkbox_list/input.tpl';
+
+    public $disabled = [];
+
+    public $emptyText = null;
+
+    public function setValue($value)
+    {
+        if ($value instanceof Model) {
+            $value = $value->id;
+        }
+        if ($value instanceof Manager) {
+            $value = $value->values(['id'], true);
+        }
+        $this->_value = $value;
+        return $this;
+    }
+}
