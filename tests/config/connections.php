b414f61a3179488329bd1a02d03043822af2bc71
Anton Okulov <agency@ordertarget.ru>
2018-08-07 15:15:27 +0300

    Connections

--- tests/config/connections.php
+++ tests/config/connections.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+return [
+    'default' => [
+        'driver' => 'mysql',
+        'config' => [
+            'host' => '127.0.0.1',
+            'database' => 'phact',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ]
+    ]
+];
