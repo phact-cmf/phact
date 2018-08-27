42fae4834b3c41baad20363b2962604f038aecd2
Anton Okulov <agency@ordertarget.ru>
2018-08-27 15:55:19 +0300

    Int field test

---
+++ tests/sandbox/app/Modules/Test/Models/Storage.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Okulov Anton
+ * @email qantus@mail.ru
+ * @version 1.0
+ * @date 12/04/16 18:51
+ */
+
+namespace Modules\Test\Models;
+
+use Phact\Orm\Fields\CharField;
+use Phact\Orm\Fields\HasManyField;
+use Phact\Orm\Fields\IntField;
+use Phact\Orm\Model;
+
+class Storage extends Model
+{
+    public static function getFields()
+    {
+        return [
+            'name' => [
+                'class' => CharField::class,
+            ],
+            'free_spaces' => [
+                'class' => IntField::class,
+                'default' => 0,
+            ]
+        ];
+    }
+}
